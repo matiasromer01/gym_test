27321c4874e3faed30c8a8e0d83a7d9e7f525d22
mati.ro <matiromero30@gmail.com>
2026-02-25 16:32:03 -0300

    profile-css-set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import ProfilePage from './pages/ProfilePage';
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   
+  return (
+    <Router>
+      <div className='App'>
+        <div className='content'>
+          
+          <Routes>
+            {/*Ruta principal-- profile  */}
+            <Route path="/" element={<ProfilePage />}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
+  
 }
+
 
 export default App
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import ProfilePic from "../components/ProfilePic.jsx";
 
 const ProfilePage = () => {
     return (
         <div>
             <div>
-                
+                <ProfilePic imageUrl="https://via.placeholder.com/150" />
+                <h2>Nombre</h2>
             </div>
         </div>
     );
 }
+
+export default ProfilePage 
